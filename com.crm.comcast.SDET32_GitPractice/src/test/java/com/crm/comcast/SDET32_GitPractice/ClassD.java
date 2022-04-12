2f3b0be5a7bd13085b7784d94bb4cbff9e746f7e
admin <admin@Divya>
2022-04-12 14:26:29 +0530

    Commit 4

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassD.java
@@ -0,0 +1,13 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassD 
+{
+	@Test
+	public void Demo4()
+	{
+		System.out.println("new Test");
+	}
+
+}
