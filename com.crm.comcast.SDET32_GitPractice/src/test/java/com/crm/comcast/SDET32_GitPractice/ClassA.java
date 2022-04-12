1721a61d23772d27459b95f809523001e47dd9e8
admin <admin@Divya>
2022-04-12 11:38:34 +0530

    Commit 1

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassA.java
@@ -0,0 +1,12 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassA
+{
+@Test
+public void DemoTest()
+{
+	System.out.println("first push");
+}
+}
